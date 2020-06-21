174616ef65c6791438026aa1d15e4ba988bd0409
Tilekbek Kadyrov <tilekbekkadyrov@gmail.com>
2020-06-21 13:43:11 -0500

    Initial Commit

---
+++ Leafy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leafy
+//
+//  Created by tilekbek kadyrov on 6/21/20.
+//  Copyright © 2020 tilekbek kadyrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
